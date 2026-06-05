ca4d68461dbdf957bde1d3d91b397950d680e185
KabankoDasha <daryaxf@icloud.com>
2026-06-05 22:10:12 +0300

    add cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -24,6 +24,14 @@ exports.register = async (req, res) => {
       { expiresIn: '7d' }
     );
 
+    res.cookie('token', token, {
+        httpOnly: true,               
+        secure: true,                  
+        sameSite: 'lax',              
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        domain: '.lider-avto74.ru'
+    });
+
     res.status(201).json({
       token,
       user: {
@@ -64,6 +72,14 @@ exports.login = async (req, res) => {
       process.env.JWT_SECRET,
       { expiresIn: '7d' }
     );
+
+    res.cookie('token', token, {
+        httpOnly: true,               
+        secure: true,                  
+        sameSite: 'lax',              
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        domain: '.lider-avto74.ru'
+    });
 
     res.json({
       token,
