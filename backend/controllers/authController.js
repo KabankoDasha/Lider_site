f3c38c71dfd6a12cfcee524ba445eeb124ed6af3
KabankoDasha <daryaxf@icloud.com>
2026-06-19 18:46:59 +0300

    fix code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,8 +7,8 @@ const nodemailer = require('nodemailer');
 // Настройка Nodemailer для Yandex
 const transporter = nodemailer.createTransport({
   host: 'smtp.yandex.ru',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER, 
     pass: process.env.EMAIL_PASS 
